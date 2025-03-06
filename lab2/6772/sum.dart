7f08c5ffa40684c5d43b0fdc52236c89def5ef9b
ramcharanb2005 <ramcharanb2005@gmail.com>
2025-03-06 10:11:44 +0530

    Delete lab2/6772 directory

--- lab2/6772/sum.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main(){
-	stdout.write("Enter val-1:");
-	int val1=int.parse(stdin.readLineSync()!);
-	stdout.write("Enter val-2:");
-	int val2=int.parse(stdin.readLineSync()!);	
-	print(val1+val2);
-}
