b2d52f9296bbbaf31760e7050c7a420588764aa0
ramcharanb2005 <ramcharanb2005@gmail.com>
2025-03-13 10:37:41 +0530

    Add files via upload

---
+++ lab3/classobject/bin/classobject.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:classobject/switchdemo.dart'as mani;
+import 'package.classobject/switchdemo.dart';
+import 'dart:io'
+void main(List<String> arguments) {
+  switchdemo sd=switchdemo();
+  sd.sdemo(stdin.readLineSync()!);
+  print('Hello world: ${mani.calculate()}!');
+  }
---
+++ lab3/classobject/lib/switchdemo.dart
@@ -0,0 +1,19 @@
+class switch_demo{
+void sdemo(String name){
+	switch(name){
+	case 'apple':
+		print('apple');
+		break;
+	case 'mango':
+		print('mango');
+	default:
+		print('sorry fruit is not listed');
+	}
+   }
+}
+		
+	
+
+	
+
+ 
