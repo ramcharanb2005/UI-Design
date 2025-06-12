a9f361354a0ae3fb84670c5261d13c778086b1f3
ramcharanb2005 <ramcharanb2005@gmail.com>
2025-06-12 09:47:54 +0530

    Create pc.dart

---
+++ lab7/pc.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class pc extends StatelessWidget {
+  const pc({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          color: Colors.blue,
+          child: Text("Sidebar"),
+        ),
+      ],
+    );
+  }
+
+
+
+}
