80920a5d70f768aa1634b4dbeeeae39185f9209b
ramcharanb2005 <ramcharanb2005@gmail.com>
2025-06-12 09:47:23 +0530

    Create mobile.dart

---
+++ lab7/mobile.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class mobiles extends StatelessWidget {
+  const mobiles({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          color: Colors.lime,
+          child: Text("down-bar"),
+        ),
+      ],
+    );
+  }
+
+
+
+}
