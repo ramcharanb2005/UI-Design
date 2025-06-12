2d06d75fc035a1f17268ea9919b03a688da4b2c1
ramcharanb2005 <ramcharanb2005@gmail.com>
2025-06-12 09:46:19 +0530

    Update main.dart

--- lab7/main.dart
+++ lab7/main.dart
@@ -1,1 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:media_query/tablet.dart';
+import 'package:media_query/mobiles.dart';
+import 'package:media_query/pc.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Media Query',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Desktop(),
+    );
+  }
+}
+
+class Desktop extends StatefulWidget {
+  const Desktop({super.key});
+  @override
+  State<Desktop> createState() => _Desktop();
+}
+
+class _Desktop extends State<Desktop> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Media Query')),
+        body: LayoutBuilder(
+            builder: (context, constraints) {
+              if (constraints.maxWidth >= 600 && constraints.maxWidth <= 1200) {
+                return tablet();
+              } else if (constraints.maxWidth < 600) {
+                return mobiles();
+              } else {
+                return pc();
+              }
+            }
+        ),
+      ),
+    );
+  }
+}
